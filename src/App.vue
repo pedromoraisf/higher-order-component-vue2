cc8a4e5678618c1fbdf7e930b6ae24ab2bda8917
pedromoraisf <pedro.morais1997@gmail.com>
2021-02-28 21:32:29 -0300

    feat: add App.vue orchestrator

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <chat-messages />
+    <send-message @click="actionInChatEventHappen" />
+  </div>
+</template>
+
+<script>
+import ChatMessages from "@/components/chat-visualization/messages";
+import SendMessage from "@/components/chat-visualization/send";
+
+import chatConversationPresentation from "@/presentation/chat-conversation";
+
+export default {
+  components: {
+    ChatMessages,
+    SendMessage
+  },
+  methods: {
+    actionInChatEventHappen(payload) {
+      const setFunction = chatConversationPresentation[payload.functionName];
+      setFunction(payload.value);
+    }
+  }
+};
+</script>
